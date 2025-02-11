0c2e1e3af9bb6571f5bd70b41ed2b7010bbba590
rumale0508 <mkamiya1026@gmail.com>
2025-02-11 10:31:30 +0900

    modify kadai007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -2,11 +2,13 @@ let num = Math.floor(Math.random() * 31);
 console.log(num);
 
 if (num == 0) {
-    console.log();
+    console.log(num);
 } else if ((num % 3 == 0) && (num % 5 == 0)) {
     console.log('3と5の倍数です')
 } else if (num % 3 == 0) {
     console.log('3の倍数です');
 } else if (num % 5 == 0) {
     console.log('5の倍数です');
+} else {
+    console.log(num);
 }
